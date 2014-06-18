c4ce7ffdfd7ec7e8d6b7b90bcb7b14a59b4a3983
Ellie Schneiders <ems2141@gmail.com>
2014-06-18 09:25:55 -0600

    it determines whether the grade went up or down or stayed even

--- lib/grader.rb
+++ lib/grader.rb
@@ -5,8 +5,23 @@ class Grader
   end
 
   def process
+    results = []
     if @data.empty?
-      []
+      results
+    else
+      @data.each_with_index do |grade, i|
+        last_grade = @data[i-1] unless i == 0
+        if last_grade != nil
+          if last_grade == grade
+            results << :even
+          elsif last_grade > grade
+            results << :down
+          else
+            results << :up
+          end
+        end
+      end
+      results
     end
   end
 end
--- spec/grader_spec.rb
+++ spec/grader_spec.rb
@@ -8,4 +8,11 @@ describe Grader do
 
     expect(Grader.new(input).process).to eq expected
   end
+
+  it 'processes whether a list of grades went up, down, or stayed the same' do
+    input = [6,3,5,4,3,4,4,5]
+    expected = [:down,:up,:down,:down,:up,:even,:up]
+
+    expect(Grader.new(input).process).to eq expected
+  end
 end
